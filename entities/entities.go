39a7aec81181fce1cd376d4d6605f8e57944ac6d
Alexander Sheiko <alexander.sheiko@mgid.com>
2020-09-12 17:17:29 +0300

    Add test entity

--- entities/entities.go
+++ entities/entities.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/n0madic/crossposter/entities/instagram"
 	_ "github.com/n0madic/crossposter/entities/rss"
 	_ "github.com/n0madic/crossposter/entities/telegram"
+	_ "github.com/n0madic/crossposter/entities/test"
 	_ "github.com/n0madic/crossposter/entities/twitter"
 	_ "github.com/n0madic/crossposter/entities/vk"
 )
---
+++ entities/test/test.go
@@ -0,0 +1,68 @@
+package test
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/n0madic/crossposter"
+	log "github.com/sirupsen/logrus"
+)
+
+// Test entity
+type Test struct {
+	entity *crossposter.Entity
+	post   crossposter.Post
+}
+
+func init() {
+	crossposter.AddEntity("test", New)
+}
+
+// New return test entity
+func New(entity crossposter.Entity) (crossposter.EntityInterface, error) {
+	test := &Test{
+		entity: &entity,
+		post: crossposter.Post{
+			Date:   time.Now(),
+			URL:    entity.Options["url"],
+			Title:  entity.Options["title"],
+			Author: entity.Options["author"],
+			Text:   entity.Options["text"],
+		},
+	}
+	if entity.Options["attachment"] != "" {
+		test.post.Attachments = []string{entity.Options["attachment"]}
+	}
+	return test, nil
+}
+
+// Get test message
+func (test *Test) Get(name string, lastUpdate time.Time) {
+	defer crossposter.WaitGroup.Done()
+
+	testLogger := log.WithFields(log.Fields{"name": name, "type": test.entity.Type})
+
+	for {
+		testLogger.Info("Check test message")
+		for _, topic := range test.entity.Topics {
+			crossposter.Events.Publish(topic, test.post)
+		}
+		time.Sleep(time.Duration(crossposter.WaitTime) * time.Minute)
+	}
+}
+
+// Post test message
+func (test *Test) Post(post crossposter.Post) {
+	log.WithFields(log.Fields{
+		"title":       post.Title,
+		"author":      post.Author,
+		"date":        post.Date,
+		"url":         post.URL,
+		"text":        post.Text,
+		"more":        post.More,
+		"attachments": post.Attachments,
+	}).Info("Test message")
+}
+
+// Handler test message
+func (test *Test) Handler(w http.ResponseWriter, r *http.Request) {}
